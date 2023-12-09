0e614c7f816635ead7eab5b772156f073ce15a9b
Daniel Turek <danielturek@gmail.com>
2023-12-09 07:28:18 -0500

    update to RW_PF_block sampler

--- packages/nimbleSMC/R/MCMCSamplers.R
+++ packages/nimbleSMC/R/MCMCSamplers.R
@@ -325,9 +325,12 @@ sampler_RW_PF_block <- nimbleFunction(
     modelLP0 <- storeParticleLP + getLogProb(model, target)
     propValueVector <- generateProposalVector()
     my_setAndCalculate$run(propValueVector)
-    particleLP <- my_particleFilter$run(m)
-    modelLP1 <- particleLP + getLogProb(model, target)
-    jump <- my_decideAndJump$run(modelLP1, modelLP0, 0, 0)
+    newLP <- getLogProb(model, target)
+    if(!is.nan(newLP) & (newLP != -Inf)) {
+        particleLP <- my_particleFilter$run(m)
+        modelLP1 <- particleLP + getLogProb(model, target)
+        jump <- my_decideAndJump$run(modelLP1, modelLP0, 0, 0)
+    } else jump <- FALSE
     if(!jump) {
       my_particleFilter$setLastLogLik(storeParticleLP)
     }
